c4fdd9a99bab9c82e6e471c90d3038d5a2374c26
0-BSCode <bryanipsanchez@gmail.com>
2024-02-21 20:45:25 +0800

    Initial commit

---
+++ apps/express-session/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/", (_req, res) => {
+    return res.json({
+        "message": "Hello World!"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("🚀Server listening on http://localhost:3000")
+})
